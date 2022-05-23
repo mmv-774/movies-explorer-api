1978429f40ed44482b897c25e9c6c309f6b95823
MIkhail <mmv-774@yandex.ru>
2022-05-23 19:34:25 +0500

    создать схемы и модели

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'Некорректный адрес эл.почты',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  { versionKey: false },
+);
+
+module.exports = mongoose.model('user', schema);
